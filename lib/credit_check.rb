904568bc3722f3b7ef220677013afd5dcfbc5dbe
Stella Mainar <mainars09@alumni.hanover.edu>
2019-03-08 18:33:53 -0500

    Add method for double_digits_are_summed

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -24,5 +24,17 @@ class CreditCheck
     end
     return doubled_digits_array
   end
-  
+
+  def double_digits_are_summed
+    doubled_digits_summed_array = []
+    every_other_digit_is_doubled.each do |digit|
+      if digit > 9
+        doubled_digits_summed_array << digit - 9
+      else
+        doubled_digits_summed_array << digit
+      end
+    end
+    return doubled_digits_summed_array
+  end
+
 end
