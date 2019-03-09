bbcf9076641d2d899747b47d0195751389dd199f
Stella Mainar <mainars09@alumni.hanover.edu>
2019-03-08 23:50:25 -0500

    Fix CreditCheck test to check that invalid card number returns correct output

--- test/credit_check_test.rb
+++ test/credit_check_test.rb
@@ -55,6 +55,11 @@ class CreditCheckTest < Minitest::Test
     assert_equal "The number 5541808923795240 is valid!", @credit_check.check_validation
 
     credit_check = CreditCheck.new("6011797668868728")
+    credit_check.single_digits
+    credit_check.every_other_digit_is_doubled
+    credit_check.double_digits_are_summed
+    credit_check.results_summed
+    credit_check.divisible_by_10?
     assert_equal "The number 6011797668868728 is invalid!", credit_check.check_validation
   end
 
