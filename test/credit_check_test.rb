57f81542801558b0dab1fe9c3aa7ba728690aee5
Stella Mainar <mainars09@alumni.hanover.edu>
2019-03-08 23:44:31 -0500

    Fixed tests for CreditCheck

--- test/credit_check_test.rb
+++ test/credit_check_test.rb
@@ -21,24 +21,29 @@ class CreditCheckTest < Minitest::Test
   end
 
   def test_it_can_double_the_value_of_every_other_digit
+    @credit_check.single_digits
     assert_equal [10, 5, 8, 1, 16, 0, 16, 9, 4, 3, 14, 9, 10, 2, 8, 0], @credit_check.every_other_digit_is_doubled
   end
 
   def test_it_can_sum_digits_greater_than_9
+    @credit_check.single_digits
+    @credit_check.every_other_digit_is_doubled
     assert_equal [1, 5, 8, 1, 7, 0, 7, 9, 4, 3, 5, 9, 1, 2, 8, 0], @credit_check.double_digits_are_summed
   end
 
   def test_it_can_sum_all_the_digits
+    @credit_check.single_digits
+    @credit_check.every_other_digit_is_doubled
+    @credit_check.double_digits_are_summed
     assert_equal 70, @credit_check.results_summed
   end
 
   def test_it_is_divisible_by_10
+    @credit_check.single_digits
+    @credit_check.every_other_digit_is_doubled
+    @credit_check.double_digits_are_summed
+    @credit_check.results_summed
     assert_equal true, @credit_check.divisible_by_10?
-  end
-
-  def test_it_can_check_validation_and_return_appropriate_message
-    assert_equal "The number 5541808923795240 is valid!", @credit_check.check_validation
-    assert_equal "The number 6011797668868728 is invalid!", @credit_check.check_validation
   end
 
 end
